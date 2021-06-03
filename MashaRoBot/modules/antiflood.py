b0a633630308d8847c75508b39326f5612b8283b
Bot-support <82792336+Bot-support@users.noreply.github.com>
2021-06-03 08:03:18 +0530

    Update antiflood.py

--- MashaRoBot/modules/antiflood.py
+++ MashaRoBot/modules/antiflood.py
@@ -419,7 +419,7 @@ will result in restricting that user.
  `1w` = 1 week
  """
 
-__mod_name__ = "Anti-flood🛡"
+__mod_name__ = "Anti-fld🛡"
 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.group, check_flood
